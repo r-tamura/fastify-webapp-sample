7eb60cd605d324d66825aca8d26b8dff93e845f0
r-tamura <rtamura30@gmail.com>
2025-03-16 02:55:40 +0000

    test: AllureでのE2Eレポート作成の仕組み導入

--- e2e/codecept.conf.ts
+++ e2e/codecept.conf.ts
@@ -1,4 +1,4 @@
-import { setCommonPlugins, setHeadlessWhen } from '@codeceptjs/configure';
+import { setCommonPlugins, setHeadlessWhen } from "@codeceptjs/configure";
 // turn on headless mode when running with HEADLESS=true environment variable
 // export HEADLESS=true && npx codeceptjs run
 setHeadlessWhen(process.env.HEADLESS);
@@ -7,18 +7,29 @@ setHeadlessWhen(process.env.HEADLESS);
 setCommonPlugins();
 
 export const config: CodeceptJS.MainConfig = {
-  tests: './*_test.ts',
-  output: './output',
+  tests: "./*_test.ts",
+  output: "./output",
   helpers: {
     Playwright: {
-      browser: 'chromium',
+      browser: "chromium",
       show: false,
     }
   },
   translation: "en-US",
   vocabularies: ["./vocabularies.json"],
   include: {
-    I: './steps_file'
+    I: "./steps_file"
   },
-  name: 'e2e'
+  name: "e2e",
+  plugins: {
+    allure: {
+      enabled: true,
+      require: "allure-codeceptjs",
+    },
+    stepByStepReport: {
+      enabled: true,
+      screenshotsForAllureReport: true,
+      deleteSuccessful: false,
+    },
+  },
 }
